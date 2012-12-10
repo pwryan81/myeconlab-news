01bf350859ab9e2a812fded43c3929f23d62df8a
Paul Ryan <pwryan81@hotmail.com>
2012-12-10 21:30:20 +1100

    Implemented tinynav.js

--- js/scripts.js
+++ js/scripts.js
@@ -5,11 +5,29 @@
  * @author: Paul Ryan
  */
 
+/** Global variables */
+var SWIPE_DURATION; 
+
+/** Detect touch device */
+if (Modernizr.touch){
+   // bind to touchstart, touchmove, etc and watch `event.streamId`
+   //alert("This is a touch device");
+   SWIPE_DURATION = 0; // Disabled the slide animation on touchscreens to increase responsiveness. User can still swipe manually.
+} else {
+   // bind to normal click, mousemove, etc
+    //alert("This is NOT a touch screen");
+	SWIPE_DURATION = 400; // On desktop, slide the tab panels 
+}
+
+
+
+
+/** Use CSS3 transforms for tab slider if browser supports it */
 if ( Modernizr.csstransforms ) {
 	  window.mySwipe = new Swipe(
 	  document.getElementById('slider'), {
 		  callback: function (e, pos) {	
-			  $('#tabs .selected').removeClass('selected');  
+		  	  $('#tabs li').removeClass('selected');  		  
 			  $('#tabs li').eq(pos).addClass('selected');
 		  }
 	});
@@ -45,19 +63,20 @@ $(document).ready(function () {
             itemSelector: '.rssItem',
             layoutMode: 'masonry',        
         });
-		// build filter buttons dynamically
 		
+		// Build filter buttons dynamically		
 		var filterButtonsHTML;
 		for (var i=0; i<australianRssFeeds.length; i++) {
 			filterButtonsHTML += '<li data-filter=".' + australianRssFeeds[i].classname + '">' + australianRssFeeds[i].displayname + '</li>';
 		}		
 		$('#tabPanel1 .filterButtonGroup').append(filterButtonsHTML); // TO DO: It adds the buttons, but don't seem to respond to their event listners
 		
-		initColorbox();
+		
+		if (!Modernizr.touch) {initColorbox();}
 		
     });	
 	/**  RSS Feeds for container2 */
-    $feedContainer2.rssfeed(worldRssFeeds, {
+/*    $feedContainer2.rssfeed(worldRssFeeds, {
         snippet: false,
         limit: 3,
         sort: 'date',
@@ -69,11 +88,12 @@ $(document).ready(function () {
             itemSelector: '.rssItem',
             layoutMode: 'masonry'
         });
-		initColorbox();
-    });	
+		// Dont use lightbox on mobile/touch devices. It screws up the page scrolling, and the phone back button exits the app completly
+		if (!Modernizr.touch) {initColorbox();}
+    });	*/
 	
 	/**  RSS Feeds for container3 */
-    $feedContainer3.rssfeed(videoRssFeeds, {
+ /*   $feedContainer3.rssfeed(videoRssFeeds, {
         snippet: false,
         limit: 3,
         sort: 'date',
@@ -84,8 +104,8 @@ $(document).ready(function () {
             itemSelector: '.rssItem',
             layoutMode: 'masonry'
         });
-		initColorbox();
-    });
+		if (!Modernizr.touch) {initColorbox();}
+    });*/
 	
 	
 	/* TAB GROUP EVENT HANDLER*/
@@ -94,7 +114,7 @@ $(document).ready(function () {
 		$(this).addClass('selected');
 		var slideNumber = $(this).index();
 		if (mySwipe) {		
-			mySwipe.slide(slideNumber,400);		//TO DO: Check if mySwipe object exists first. Not in IE8.
+			mySwipe.slide(slideNumber,SWIPE_DURATION);		//TO DO: Check if mySwipe object exists first. Not in IE8.
 		}
 	});
 	
@@ -107,7 +127,7 @@ $(document).ready(function () {
 		// Change selected button state		
 		$(this).parent().find('.selected').removeClass('selected');
 		$(this).addClass('selected');
-		    $(this).parent().siblings('.feedContainer').isotope({
+		$(this).parent().siblings('.feedContainer').isotope({
             filter: selector
         });
     });
@@ -130,7 +150,7 @@ $(document).ready(function () {
         $('.feedContainer').removeClass('tileview').removeClass('gridview').addClass('imageView').isotope('reLayout');
     });
 	
-	
+	/** Lightbox */
 	function initColorbox()
 	{
 		$(".iframe").colorbox({
@@ -143,6 +163,15 @@ $(document).ready(function () {
 			onClosed:function(){ /*alert('onClosed: colorbox has completely closed'); /*window.location.href = history.back(); */}
 	  }); 
 	}
+	
+	
+	 // TinyNav.js 1
+      $('#nav').tinyNav({
+        active: 'selected'
+      });
+	
 
 	
-});
+
+	
+}); // End ready()
